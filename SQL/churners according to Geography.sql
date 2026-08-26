fde88cb97ba8da80225bacba941f4e58043e4c95
Tushr singh <singhtusharji522@gmail.com>
2026-08-26 12:27:21 +0530

    Enhance churners query with total and rate
    Updated SQL query to include total customers and churn rate calculation.

--- SQL/churners according to Geography.sql
+++ SQL/churners according to Geography.sql
@@ -1,8 +1,9 @@
 -- Churners and non_churners according to geography
-     select 
-      geography, 
+       select 
+      geography, count(*) as Total_customers,
       count(case when exited = 'yes' then 1 end) as churner, 
-      count(case when exited = 'no' then 1 end) as non_churners 
+      count(case when exited = 'no' then 1 end) as non_churners,
+      round(count(case when exited = 'yes' then 1 end) * 100.0/count(*),2) as Rate
       from 
        bank_churn 
       group by 
